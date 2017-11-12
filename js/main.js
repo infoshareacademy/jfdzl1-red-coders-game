74a6772feddeb2b4c98dec5582c31e61c85f4ea9
Jakub Fedyk <jakubfedyk@gmail.com>
2017-11-12 16:48:23 +0100

    creating instructions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+
+    var element = document.createElement('div');
+    element.id = 'hero';
+    element.width = '20px';
+    element.height = '20px';
+    element.style.backgroundColor = '#FF0';
+
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(element);
+)
+}
+''
