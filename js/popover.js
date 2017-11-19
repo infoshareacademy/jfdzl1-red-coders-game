3ee5b0d3379d447af03585c38874e4133b7b619e
Marcin Domański <marcin@domanski.eu>
2017-11-19 12:21:55 +0100

    15644-corrected position and text

--- js/popover.js
+++ js/popover.js
@@ -1,7 +1,3 @@
-// $(document).ready(function(){
-//     $('[data-toggle="popover"]').popover();
-// });
-
 $('.popuptext').hide();
 $(".help").on('click', function () {
     $('.popuptext').show();
