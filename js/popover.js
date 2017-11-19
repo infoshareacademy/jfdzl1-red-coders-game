c76cd8cc4f2be8c8e12bf2f8ec0b07e5692c8ee7
Marcin Domański <marcin@domanski.eu>
2017-11-19 09:34:02 +0100

    15644-modified popover

--- js/popover.js
+++ js/popover.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover();
+// $(document).ready(function(){
+//     $('[data-toggle="popover"]').popover();
+// });
+
+$('.popuptext').hide();
+$(".popup").on('click', function () {
+    $('.popuptext').show();
 });
+$('.popuptext').on('mouseleave', function() {
+    $(this).hide();
+})
